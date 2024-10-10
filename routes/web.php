ffa15c172c6e063b079f9399fea47424f01cb672
harmostays <harmostays@gmail.com>
2024-10-10 13:18:50 +0000

    db backup

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-    /*
-    |--------------------------------------------------------------------------
-    | Web Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register web routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
-    |
-    */
-Route::get('/intro','LandingpageController@index');
+// Other routes...
+
+Route::get('/intro', 'LandingpageController@index');
 Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/install/check-db', 'HomeController@checkConnectDatabase');
@@ -23,12 +14,17 @@ Route::get('social-login/{provider}', 'Auth\LoginController@socialLogin');
 Route::get('social-callback/{provider}', 'Auth\LoginController@socialCallBack');
 
 // Logs
-Route::get(config('admin.admin_route_prefix').'/logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')->middleware(['auth', 'dashboard','system_log_view'])->name('admin.logs');
+Route::get(config('admin.admin_route_prefix') . '/logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')->middleware(['auth', 'dashboard', 'system_log_view'])->name('admin.logs');
 
-Route::get('/install','InstallerController@redirectToRequirement')->name('LaravelInstaller::welcome');
-Route::get('/install/environment','InstallerController@redirectToWizard')->name('LaravelInstaller::environment');
+Route::get('/install', 'InstallerController@redirectToRequirement')->name('LaravelInstaller::welcome');
+Route::get('/install/environment', 'InstallerController@redirectToWizard')->name('LaravelInstaller::environment');
 
-
+// Admin routes
 Route::group(['prefix' => 'admin'], function () {
+    // Custom admin index route
+    Route::get('/', 'AdminController@index')->name('admin.index'); // Define the admin index route
+
+    // Voyager routes
     Voyager::routes();
 });
+
